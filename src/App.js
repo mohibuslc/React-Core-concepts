573908f8658116d6ebb8768dfee5e2163c5cc6ce
mohibuslc <mohib_uslc@outlook.com>
2021-03-09 23:36:41 +0000

    first type in React+App.JS

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person  = {
+    name :" De. Sha",
+
+    job:"Singer"
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit Make done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My First React Paragraph </p>
+        <h1> My Heading is : {person.name +" "+ person.job}</h1>
       </header>
     </div>
   );
